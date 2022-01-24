cb016262f3ff657b880ef1908c3a0bc3bc5811ea
John Doe <davtarro@gmail.com>
2022-01-24 10:02:03 +0100

    f

---
+++ main.js
@@ -0,0 +1,7 @@
+document.getElementById("monnom").onmouseover= function(Event){
+       this.style.color='blue';
+    }
+   
+document.getElementById("monnom").onmouseout= function(Event){
+       this.style.color='grey';
+        }
